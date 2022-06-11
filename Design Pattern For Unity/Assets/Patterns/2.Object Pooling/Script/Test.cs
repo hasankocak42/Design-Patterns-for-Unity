dd076cc7523b755af8204df396504970923719cd
hasankocak42 <hasankckia@gmail.com>
2022-06-11 18:06:33 +0300

    ObjectPool Added

---
+++ Design Pattern For Unity/Assets/Patterns/2.Object Pooling/Script/Test.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+   
+    [SerializeField] private float spawnInterval = 1;
+
+    [SerializeField] private ObjectPool objectPool = null;
+    void Start()
+    {
+        StartCoroutine(nameof(SpawnRoutine));
+    }
+
+    private IEnumerator SpawnRoutine()
+    {
+        int counter = 0;
+        while (true)
+        {
+
+           GameObject obj = objectPool.GetPooledObject(counter++ % 5);
+           obj.transform.position = Vector3.zero;
+            
+            yield return  new WaitForSeconds(spawnInterval);
+        }
+    }
+
+}
